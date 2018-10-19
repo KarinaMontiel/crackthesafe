7105818bbcd035e7b4a46df9f256df2dad1c01a8
Karina Montiel <karinamontiel375@gmail.com>
2018-10-19 01:23:37 +0000

    your message here

--- js/script.js
+++ js/script.js
@@ -8,4 +8,23 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+//document.ready(function(){
+var answer1, answer2, answer3, answer4;
+
+var answer1= $("#number1").val(Math.floor(Math.random()*10));
+var answer2= $("#number2").val(Math.floor(Math.random()*10));
+var answer3= $("#number3").val(Math.floor(Math.random()*10));
+var num4= $("#number4").val(Math.floor(Math.random()*10));
+console.log(num1);
+
+
+$("#verify").click(function(){
+    
+    $("#userChoice").append($("#number1").val());
+    $("#userChoice").append($("#number2").val());
+    $("#userChoice").append($("#number3").val());
+    $("#userChoice").append($("#number4").val());
+
+    
+});
 
